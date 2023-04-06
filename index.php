49bda1786e5c56d19593ee3dd140861cdd9cff53
htmlcoder001 <htmlcoder001@gmail.com>
2023-04-06 13:20:21 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,291 @@
+<?php  require('onepage.php'); ?>
+<!DOCTYPE html>
+<html>
+    <head>      
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width">       
+        <title>Купить ирригатор Power Floss </title>
+        <meta name="description" content="ИРРИГАТОР POWER FLOSS ДЛЯ ЗУБОВ">
+        <meta name="keywords" content="Power Floss, ирригатор, ирригатор для зубов, ЛУЧШЕ ПРОФИЛАКТИКА – ЧЕМ ДОЛГОЕ, БОЛЕЗНЕННОЕ И ДОРОГОЕ ЛЕЧЕНИЕ">
+        <link rel="stylesheet" href="css\settings.css" type="text/css">
+        <link rel="stylesheet" href="css\slick.css" type="text/css">
+        <link rel="stylesheet" href="css\main.css" type="text/css">
+        <link rel="stylesheet" type="text/css" href="css\roboto.css">
+        
+        <link rel="stylesheet" type="text/css" href="css\slick.css">
+        <link rel="stylesheet" type="text/css" href="css\slick-theme.css">
+        <link href="css\custom-styless.min.css" type="text/css" rel="stylesheet">
+        <meta property="og:title" content="ИРРИГАТОР POWER FLOSS"/>
+        <meta property="og:description" content="ЛУЧШЕ ПРОФИЛАКТИКА – ЧЕМ ДОЛГОЕ,
+БОЛЕЗНЕННОЕ И ДОРОГОЕ ЛЕЧЕНИЕ"/>
+        <meta property="og:image" content="img/tov.png"/>
+                
+        <link rel="shortcut icon" href="favicon85.ico">
+    </head>
+<body>
+                <section class="block-1">
+            <div class="wrap">
+                <h1>Купить ирригатор Power Floss</h1>
+                <div class="container">
+                    <h2 class="title">Лучше профилактика – чем долгое,<br>болезненное и дорогое лечение</h2>
+                    <ul class="list">
+                        <li>Полностью безопасен </li>
+                        <li>Нет пропусков при чистке ирригатором</li>
+                        <li>Работает без батареек и электричества</li>
+                        <li>Всегда готов к работе</li>
+                    </ul>
+                    <div class="cost clearfix">
+                        <span class="old-cost">
+                           Старая цена:<br>
+                            <span class="price_old3834"><?= $oldPriceHtml ?> <?= $currencyDisplayHtml ?></span><span class="price_currency"></span>
+                        </span>
+                        <span class="new-cost">
+                            Новая цена:<br>
+                            <span class="price_only3834"><?= $newPriceHtml ?> <?= $currencyDisplayHtml ?></span><span class="price_currency"></span>
+                        </span>
+                    </div>
+                    <a class="button-m" href="#zakaz">Заказать со скидкой</a>
+                </div>
+            </div>
+        </section>
+
+         <section class="block-3">
+                    <div class="wrap">
+                        <div class="use">
+                            <h2 class="title">Варианты использования Ирригатора Power Floss</h2>
+                            <ul class="clearfix">
+                                <li>
+                                    <img src="img\use-1.png" alt="Power Floss">
+                                    ДЛЯ ИМПЛАНТОВ
+                                </li>
+                                <li>
+                                    <img src="img\use-2.png" alt="Power Floss">
+                                    ДЛЯ БРЕКЕТ-СИСТЕМ
+                                </li>
+                                <li>
+                                    <img src="img\use-3.png" alt="Power Floss">
+                                    ДЛЯ КОРОНОК
+                                </li>
+                                <li>
+                                    <img src="img\use-4.png" alt="Power Floss">
+                                    ВМЕСТО ЗУБНОЙ НИТИ
+                                </li>
+                            </ul>
+                          
+                        </div>    
+                    </div>
+        </section>
+
+         <section class="block-stress">
+            <div class="wrap">
+                <h2 class="title">ИЗБАВЬТЕ СЕБЯ ОТ СТРЕССОВ, СВЯЗАННЫХ С ПОСТОЯННЫМ ПОСЕЩЕНИЕМ СТОМАТОЛОГА</h2>
+                <div class="stress"><img src="img\iriggat.jpg" alt="Power Floss"></div>   
+
+                <a class="button-m" href="#zakaz">Заказать со скидкой</a>
+            </div>    
+        </section>
+
+
+        <section class="block-2">
+            <div class="wrap">
+                <h2 class="title">Преимущества использования ирригатора</h2>
+                <div class="sizes">
+                    <table>
+                        <tr>
+                            <td>    Удаляет бактерии, налёт и остатки пищи из труднодоступных мест между зубами и деснами</td>
+                        </tr>
+                        <tr>
+                            <td>     Струя проникает в пространство между зубами и в дёсневые карманы, куда не могут добраться зубная щетка и зубная нить</td>
+                        </tr>
+                        <tr>
+                            <td>Эффективная профилактика зубного камня</td>
+                        </tr>
+                        <tr>
+                            <td>Напор и температура воды регулируется самостоятельно</td>
+                        </tr>
+                        <tr>
+                            <td>Лучшее средство для борьбы с кровоточивостью дёсен, улучшает кровообращение в дёснах. Идеально подходит для чистки протезов, имплантов, брекет-систем и мостов</td>
+                        </tr>
+                          <tr>
+                            <td>Ирригатор идеально подходит для улучшения состояния дёсен при пародонтите и пародонтозе, а также профилактики этих заболеваний во время беременности и кормления, при сахарном диабете, у курильщиков</td>
+                        </tr>
+                          <tr>
+                            <td>Устраняет проблемы запаха изо рта</td>
+                        </tr>
+                    </table>
+                      
+                </div>
+        </div></section>
+
+         <section class="block-option">
+            <div class="wrap">
+                <h2 class="title">ХАРАКТЕРИСТИКИ Ирригатора Power Floss</h2>
+
+                <div class="itemBlock">
+                    <div class="left">
+                        <img src="img\tov.png" alt="Power Floss">
+                    </div>
+                    <div class="right">
+                        <div class="first-right">
+                        <ul>
+                            <li>
+                                <h3>Количество уровней регулировки напора воды:</h3>
+                                <p>1</p>
+                            </li>
+                            <li>
+                                <h3>Объём резервуара для воды: </h3> <p>50мл</p>
+                                
+                            </li>
+                            <li>
+                                <h3>Количество насадок:</h3>
+                                <p>1</p>
+                            </li>
+                            <li>
+                                <h3>Источник питания:</h3>
+                                <p>Не требуется</p>
+                            </li>
+                            <li>
+                                <h3>Материал:</h3>
+                                <p>Пластик</p>
+                            </li>
+                            <li>
+                                <h3>Размер упаковки, см: </h3>
+                                <p>23 х 6,7 х 4,7</p>
+                            </li>
+                            <li>
+                                <h3>Вес:</h3>
+                                <p>210 г</p>
+                            </li>
+                        </ul>
+
+
+              </div>
+            </div> 
+              </div></div></section>
+              <div class="wrap"> 
+             <div class="slider_1"> 
+                    <div class="gal">   
+                   <div class="slide_1"> <img src="img\prod-1.jpg"></div>  
+              </div>
+              </div>
+              </div>
+      
+
+
+      <section class="block-option">
+            <div class="wrap">
+                <h2 class="title">Принцип работы ИРРИГАТОРА Power Floss</h2>
+
+                <div class="itemBlock">
+                    <div class="left">
+                        <img src="img\tov.jpg" alt="Power Floss">
+                    </div>
+                    <div class="right">
+                        <ul>
+
+                            <li>Откройте клапан резервуара и залейте в ирригатор очищенную воду</li>
+                            <li>Поднесите прибор к ротовой полости и расположите под углом 60–90 градусов</li>
+                            <li>Ритмично нажимайте на кнопку, расположенную на корпусе, направляя поток воды в нужном направлении</li>
+                            <li>По окончании процедуры удалите из резервуара остатки воды и уберите прибор до следующего применения</li>
+                        </ul>
+                    </div>
+                </div>
+        
+            </div>    
+        </section>
+
+
+        <section class="block-4">
+            <div class="wrap clearfix">
+                <div class="container left">
+                    <h2 class="title">Как сделать заказ</h2>
+                    <ul class="deliv clearfix">
+                        <li>
+                            <span>1</span>
+                            <p>Вы оставляете заявку, заполнив форму заказа на нашем сайте</p>
+                        </li>
+                        <li>
+                            <span>2</span>
+                            <p>Менеджер перезванивает вам для уточнения деталей заказа</p>
+                        </li>
+                        <li>
+                            <span>3</span>
+                            <p>Получаете и оплачиваете заказ на почте</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container right">
+                    <h2 class="title">Наши преимущества</h2>
+                    <ul class="sup clearfix">
+                        <li>
+                            <h3>Без предоплат</h3>
+                            <p>Оплата заказа осуществляется при получении на руки</p>
+                        </li>
+                        <li>
+                            <h3>Быстрая доставка</h3>
+                            <p>Доставка от 3х дней</p>
+                        </li>
+                        <li>
+                            <h3>Гарантия качества</h3>
+                            <p>Даже если Вы найдете заводской брак, мы поменяем товар на новый</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+        <section class="block-6" id="block66">
+            <div class="wrap">
+                <h2 class="title">Отзывы довольных покупателей</h2>
+                <div class="rev-slider clearfix">
+                    <div class="rev-item">
+                        <img src="img\rep-1.jpg" alt="Power Floss">
+                        <h4>Юлия</h4>
+                        <p>За 2 года использования новых кариесов не возникло, десна абсолютно здоровая. Дыхание свежее. Профессиональную гигиену не требовалось делать все это время. Камней не образовалось. Вот и все, что потребовалось от стоматолога. Если посчитать стоимость такого "лечения" , становится понятно что это серьёзная экономия денег, времени и нервов (для тех кто не любит походы к стоматологу).</p>
+                    </div>
+                    <div class="rev-item">
+                        <img src="img\rep-2.jpg" alt="Power Floss">
+                        <h4>Екатерина</h4>
+                        <p>Незаменимая вещь - зубная нить не сравнится, постоянно рвётся и такого эффекта не производит, а именно между зубами чаще всего образуется кариес, потому что щетка там всего не вычищает. Теперь у меня все уже в пломбах, но после ирригатора приятное ощущение свежести во рту и профессиональную чистку приходится делать гораздо реже. Отличный семейный девайс!</p>
+                    </div>
+                    <div class="rev-item">
+                        <img src="img\rep-3.jpg" alt="Power Floss">
+                        <h4>Алексей</h4>
+                        <p>Пользуюсь​ ​​ирригатором третий месяц. От системы в восторге! Ношу брекет​ ​систему и раньше была необходимость два раза в месяц ездить к ортодонту и менять резинки, так как спустя две недели они имели свойство темнеть. При использовании ирригатора, необходимость в столь частой замене резинок отпала сама собой. Превосходно очищает межзубное пространство, а также очень удобен в использовании. </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    
+
+
+        <section class="block-8">
+            <div class="wrap">
+                <h2 class="title">Закажите Power Floss сегодня со скидкой 50%</h2>
+                <div class="container clearfix">
+                    <div class="cost clearfix">
+                        <span class="old-cost">
+                            Старая цена:<br>
+                            <span class="price_old3834"><?= $oldPriceHtml ?> <?= $currencyDisplayHtml ?></span><span class="price_currency"></span>
+                        </span>
+                        <span class="new-cost">
+                            Новая Цена:<br>
+                            <span class="price_only3834"><?= $newPriceHtml ?> <?= $currencyDisplayHtml ?></span><span class="price_currency"></span>
+                        </span>
+                    </div>
+                    <div class="order" id="zakaz">
+                        <form class="m1-form" action="" method="post">
+                        <div><?= countrySelect() ?></div>
+                        <div><input type="text" name="name" placeholder="Введите Ф.И.О"  class="-metrika-nokeys"></div> <div><input type="text" name="phone" id="phone1" placeholder="Введите телефон"  class="input-phone"></div> <div class="footxt"><p>  </p></div><div><button class="button-m" type="submit">Заказать</button></div> 
+                                                                                                                            
+                    </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="copyright">
+             <?= footer() ?>
+        </section>
+</body>
+</html>
